7054bcad75a5444c1d0a20a93425175d9fab8785
DalianrKholin <osadowskikacper@gmail.com>
2024-06-01 19:36:05 +0200

    create new super library

--- setUP.go
+++ setUP.go
@@ -1,27 +1,37 @@
 package superLib
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 var (
-	fileName = ""
-	handler  *os.File
+	fileName  = ""
+	handler   *os.File
+	dbConnect *sql.DB
 )
 
 func Exec(s ...string) {
-	var r string
-	for _, v := range s {
-		r += v
+	r := s[0]
+	for _, v := range s[1:] {
+		strings.Replace(r, "?", v, 1)
 	}
 	handler.Write([]byte(r + "\n"))
+
 	fmt.Printf("%v\n", r)
 }
 
-func SetUpLogger(file string) error {
+func SetUpLogger(file, connectionString string) error {
 	var err error
 	handler, err = os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbConnect = db
 	return err
 }
 
